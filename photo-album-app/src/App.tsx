1b1ae926b0fa5ab74812016bd46e4e0bd8ea8306
dkasprzak <dominik.kasprzak1408@gmail.com>
2022-11-25 19:14:40 +0100

    add style

--- photo-album-app/src/App.tsx
+++ photo-album-app/src/App.tsx
@@ -1,26 +1,27 @@
 import { FC } from 'react';
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
+import { Navbar } from './Components/Navbar/navbar';
 import { Home } from './Components/Routes/Home/home';
 import { Users } from './Components/Routes/Users/users';
 import { Posts } from './Components/Routes/Posts/posts';
 
 export const App : FC = () => {
   return (
+  <>
     <BrowserRouter>
-        <div>
-          <>{console.log('test')}</>;
+      <Navbar/>
           <Link to="Posts">Posts</Link>
           <Link to="Users">Users</Link>
           <Link to="/">Home</Link>
-        </div>
-        <Routes>
-          <Route path='Routes' element={<Users/>}/>
-          <Route path='Posts' element={<Posts/>}/>
-          <Route index element = {<Home/>}/>
-        </Routes>
+      <Routes>
+        <Route path='Routes' element={<Users/>}/>
+        <Route path='Posts' element={<Posts/>}/>
+        <Route index element = {<Home/>}/>
+      </Routes>
     </BrowserRouter>
+  </>  
   );
-};
+}
 
 export default App;
